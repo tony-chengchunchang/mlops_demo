b53b4db89a592bafb0c49d0f58f39e652359ef7e
undeep0224@gmail.com <undeep0224@gmail.com>
2023-01-16 07:30:56 +0000

    added conf and updated deployment scripts

--- pipelines/train_model.py
+++ pipelines/train_model.py
@@ -1,4 +1,10 @@
 # Databricks notebook source
+dbutils.widgets.combobox('stage', 'dev', ['dev', 'staging', 'prod'])
+stage = dbutils.widgets.get('stage')
+
+# COMMAND ----------
+
+import common
 import mlflow
 from mlflow.tracking import MlflowClient
 from sklearn.model_selection import train_test_split
@@ -21,6 +27,12 @@ def create_train_test_split(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=24)
     return X_train, X_test, y_train, y_test
 
+def get_model_params():
+    with open('model_params.json') as f:
+        params = json.load(f)
+        
+    return params
+    
 def create_training_pipeline(**params):
     preproccesor = ColumnTransformer(
         [('scaler', MinMaxScaler(), make_column_selector(dtype_exclude='object'))],
@@ -32,10 +44,12 @@ def create_training_pipeline(**params):
     )
     return pipeline
 
-def run():
+def run_training():
     with mlflow.start_run() as run:
+        mlflow.autolog()
         data = get_dataset()
         X_train, X_test, y_train, y_test = create_train_test_split(data)
+        params = get_model_params()
         model = create_training_pipeline(**params)
         model.fit(X_train, y_train)
 
@@ -45,14 +59,21 @@ def run():
         r2 = r2_score(y_test, pred)
 
         mlflow.log_metrics({'mae':mae, 'mse':mse, 'r2':r2, 'loss': -r2})
+    
+    return run
+
+def register_model(model_name, run):
+    model_uri = 'runs:/{}/model'.format(run.info.run_id)
+    model_ver = mlflow.register_model(model_uri, model_name)
+    return model_ver
+
+def main():
+    env = common.get_env(stage)
+    run = run_training()
+    register_model(env['model_name'], run)
 
 if __name__ == '__main__':
-    run()
-
-# COMMAND ----------
-
-# with_tuning
-# model_name
+    main()
 
 # COMMAND ----------
 
